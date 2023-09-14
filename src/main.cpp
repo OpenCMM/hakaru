76455d86e1ead358adcf1d97bcf892956312a79d
Yuichiro Aoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-09-14 17:00:45 +0900

    Power saving (#1)
    * stream data when needed
    
    * use touch sensor to wake up

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,49 @@ void ledBlink();
 const char* ssid = "<ssid>";
 const char* password =  "<password>";
 const float slowResponseTime = 10.0;
+const int touchPin = 4; // GPIO4 as the touch-sensitive pin
 
 WebSocketsServer webSocket(81);
 
+bool streaming = false;
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      streaming = false; // Stop streaming data when a client disconnects
+      break;
+    case WStype_TEXT:
+      if (strcmp((char*)payload, "startStreaming") == 0) {
+        streaming = true; // Start streaming data when "startStreaming" message is received
+        Serial.println("Streaming started");
+      } else if (strcmp((char*)payload, "stopStreaming") == 0) {
+        streaming = false; // Stop streaming data when "stopStreaming" message is received
+        Serial.println("Streaming stopped");
+      } else if (strcmp((char*)payload, "deepSleep") == 0) {
+        streaming = false;
+        Serial.println("Going to sleep now");
+        delay(1000);
+        esp_deep_sleep_start();
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+void touchCallback() {
+  // wake up from deep sleep
+  Serial.println("Touch detected");
+  ledBlink();
+}
+
 void setup() {
   Serial.begin(115200);
+
+  touchAttachInterrupt(touchPin, touchCallback, 40); // Attach touch interrupt
+
+  //Configure Touchpad as wakeup source
+  esp_sleep_enable_touchpad_wakeup();
 
   // connect to WiFi
   WiFi.begin(ssid, password);
@@ -28,17 +66,20 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
   webSocket.loop();
 
-  // Generate a random number here
-  int randomNum = mockGetSensorData();
-
-  // broadcast sensor data to all connected clients
-  String randomNumStr = String(randomNum);
-  webSocket.broadcastTXT(randomNumStr.c_str(), randomNumStr.length());
+  // If streaming is enabled, get sensor data and send it to the client
+  if (streaming) {
+    int randomNum = mockGetSensorData();
+    String randomNumStr = String(randomNum);
+    Serial.println(randomNumStr);
+    webSocket.broadcastTXT(randomNumStr.c_str(), randomNumStr.length());
+    delay(1000);
+  }
 }
 
 int mockGetSensorData() {
