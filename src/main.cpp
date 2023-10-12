d9bc3c727f5feebe4baa000bc6dea7050b80b7c5
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-10-12 10:10:13 +0900

    control sensor power

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 
-int mockGetSensorData();
+int getSensorData();
 void ledBlink();
+void switchSensor(bool on);
 
 const char* ssid = "<ssid>";
 const char* password =  "<password>";
 const float slowResponseTime = 10.0;
 const int touchPin = 4; // GPIO4 as the touch-sensitive pin
-const int analogPin = 35; // for sensor data
+const int sensorControlPins[3] = {5, 18, 19};
 
 WebSocketsServer webSocket(81);
 
@@ -21,7 +22,7 @@ bool streaming = false;
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
   switch (type) {
     case WStype_DISCONNECTED:
-      streaming = false; // Stop streaming data when a client disconnects
+      // streaming = false; // Stop streaming data when a client disconnects
       break;
     case WStype_TEXT:
       if (strcmp((char*)payload, "startStreaming") == 0) {
@@ -34,6 +35,9 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
         streaming = false;
         Serial.println("Going to sleep now");
         delay(1000);
+        // turn off sensor
+        switchSensor(false);
+        delay(1000);
         esp_deep_sleep_start();
       }
       break;
@@ -45,6 +49,8 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 void touchCallback() {
   // wake up from deep sleep
   Serial.println("Touch detected");
+  // turn on sensor
+  switchSensor(true);
   ledBlink();
 }
 
@@ -58,6 +64,10 @@ void setup() {
     while (1);
   }
 
+  for (int i = 0; i < 3; i++) {
+    pinMode(sensorControlPins[i], OUTPUT);
+  }
+  switchSensor(true);
   touchAttachInterrupt(touchPin, touchCallback, 40); // Attach touch interrupt
 
   //Configure Touchpad as wakeup source
@@ -85,16 +95,16 @@ void loop() {
 
   // If streaming is enabled, get sensor data and send it to the client
   if (streaming) {
-    int data = mockGetSensorData();
+    int data = getSensorData();
     String dataStr = String(data);
     Serial.println(dataStr);
     webSocket.broadcastTXT(dataStr.c_str(), dataStr.length());
     delay(200);
   }
+  delay(1000);
 }
 
-int mockGetSensorData() {
-  // int data = analogRead(analogPin);
+int getSensorData() {
   int16_t adc0;
   float volts0;
 
@@ -118,4 +128,11 @@ void ledBlink() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
+}
+
+// Switch sensor
+void switchSensor(bool on) {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(sensorControlPins[i], on ? HIGH : LOW);
+  }
 }
